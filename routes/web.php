cd9e422bbd35033a8759f795e888db9ddf983c2a
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-13 16:33:44 +0100

    create migrate

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Guest\ComicController as ComicController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-});
+Route::get('/', [ComicController::class, 'index']);
