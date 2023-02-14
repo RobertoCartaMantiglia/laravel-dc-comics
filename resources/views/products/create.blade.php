32e703c6f39104f15ea7ba768ae36875fb1161b8
RobertoCartaMantiglia <rcartmantiglia@gmail.com>
2023-02-14 15:40:39 +0100

    create form

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -10,46 +10,48 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <div class="mb-3">
-                    <label for="title" class="form-label">
-                        Titolo
-                    </label>
-                    <input type="text" name="title" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="description" class="form-label">Descrizione</label>
-                    <textarea class="form-control" name="description" rows="3"></textarea>
-                </div> 
-                <div class="mb-3">
-                    <label for="thumb" class="form-label">
-                        Immagine
-                    </label>
-                    <input type="text" name="thumb" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="price" class="form-label">
-                        Prezzo
-                    </label>
-                    <input type="number" name="price" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="series" class="form-label">
-                        Serie
-                    </label>
-                    <input type="text" name="series" class="form-control">
-                </div>
-                <div class="mb-3">
-                    <label for="sale_date" class="form-label">
-                        Data di Vendita
-                    </label>
-                    <input type="date" name="sale_date" class="form-control">
-                </div> 
-                <div class="mb-3">
-                    <label for="type" class="form-label">
-                        Tipologia
-                    </label>
-                    <input type="text" name="type" class="form-control">
-                </div>              
+                <form action="{{route('product.store')}}" method="POST">
+                    <div class="mb-3">
+                        <label for="title" class="form-label">
+                            Titolo
+                        </label>
+                        <input type="text" name="title" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="description" class="form-label">Descrizione</label>
+                        <textarea class="form-control" name="description" rows="3"></textarea>
+                    </div> 
+                    <div class="mb-3">
+                        <label for="thumb" class="form-label">
+                            Immagine
+                        </label>
+                        <input type="text" name="thumb" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="price" class="form-label">
+                            Prezzo
+                        </label>
+                        <input type="number" name="price" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="series" class="form-label">
+                            Serie
+                        </label>
+                        <input type="text" name="series" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="sale_date" class="form-label">
+                            Data di Vendita
+                        </label>
+                        <input type="date" name="sale_date" class="form-control">
+                    </div> 
+                    <div class="mb-3">
+                        <label for="type" class="form-label">
+                            Tipologia
+                        </label>
+                        <input type="text" name="type" class="form-control">
+                    </div> 
+                </form>             
             </div>
         </div>
     </div>
